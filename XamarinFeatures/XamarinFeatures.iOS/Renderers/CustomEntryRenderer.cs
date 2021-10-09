383ff78926f123a6bcf652ae47ca68ffbe350034
DeFreitas-Samuel <52897285+DeFreitas-Samuel@users.noreply.github.com>
2021-10-09 00:54:57 -0400

    Added customer renders that modify the background color of the entries

---
+++ XamarinFeatures/XamarinFeatures.iOS/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,24 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+namespace XamarinFeatures.iOS.Renderers
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.BackgroundColor = UIColor.Blue;
+            }
+        }
+    }
+}
